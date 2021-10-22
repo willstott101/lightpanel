2e11539bb560f814bfac92efa55346e9d6b58e4f
Will Stott <willstott101@gmail.com>
2021-10-22 18:09:28 +0100

    Fix HMR during development

--- frontend/index.js
+++ frontend/index.js
@@ -1,8 +1,13 @@
 /* Add JavaScript code here! */
 import App from "./App.svelte";
 
-let app = new App({
-  target: document.body,
+const root = document.getElementById("app-root");
+// Clear previous additions (via HMR)
+while (root.lastChild)
+    root.removeChild(root.lastChild);
+
+const app = new App({
+  target: root,
 });
 
 export default app;
