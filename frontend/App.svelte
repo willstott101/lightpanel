138772675783f8c3b213ef539db8692ef2d75905
Will Stott <willstott101@gmail.com>
2021-10-22 23:34:18 +0100

    Add a start/stop button.

--- frontend/App.svelte
+++ frontend/App.svelte
@@ -1,5 +1,18 @@
 <script>
     import Visualization from "./Visualization.svelte";
+    import Button from "./widgets/Button.svelte";
+    import MdPlay from "svelte-icons/md/MdPlayArrow.svelte";
+    import MdStop from "svelte-icons/md/MdStop.svelte";
+    import { executor } from './globals';
+
+    let running = true;
+    function togglePause () {
+        running = !running;
+        if (running)
+            executor.start();
+        else
+            executor.stop();
+    }
 </script>
 
 <style>
@@ -12,4 +25,11 @@
 
 <div class="root">
     <Visualization></Visualization>
+    <Button on:click={togglePause}>
+        {#if running}
+            <MdStop/>
+        {:else}
+            <MdPlay/>
+        {/if}
+    </Button>
 </div>
---
+++ frontend/widgets/Button.svelte
@@ -0,0 +1,40 @@
+<style>
+    .base {
+        background-color: #d9d6d0;
+        width: 3em;
+        height: 3em;
+        padding: 1em;
+        border-radius: 0.3em;
+        margin: 0.25em;
+        display: block;
+    }
+    .base:active:hover {
+        box-shadow: inset 1px 2px 6px 0px #00000052;
+    }
+    .knob {
+        background-color: #d9d6d0;
+        width: 100%;
+        height: 100%;
+        margin: 0;
+        display: block;
+        border-radius: 50%;
+        box-shadow:
+            8px 1px 20px 0px rgb(0 0 0 / 20%),
+            2px 4px 5px 1px rgb(0 0 0 / 20%),
+            inset 1px 2px 3px 0px #ffffff57;
+    }
+    .icon {
+        text-align: center;
+        padding: 0.2em;
+        box-sizing: border-box;
+        font-size: 1.6em;
+        color: grey;
+        user-select: none;
+    }
+</style>
+
+<div class="base" on:click>
+    <div class="knob icon">
+        <slot></slot>
+    </div>
+</div>
