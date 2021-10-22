f53f297c2d39f0efbf00d826704b02e6e1bd7ef9
Will Stott <willstott101@gmail.com>
2021-10-22 18:45:45 +0100

    indexSnake is now a hydra

--- patterns/indexSnake.js
+++ patterns/indexSnake.js
@@ -3,8 +3,9 @@ import { quantize } from "../engine/math.js";
 
 export default {
     config: {
-        period: 2,
-        trail: 1000,
+        period: 50,
+        trail: 10,
+        count: 5,
         primaryColor: {
             r: 255,
             g: 120,
@@ -17,20 +18,28 @@ export default {
         },
     },
     global: (p, c) => {
-        const index = quantize(p.time * p.length, c.period, p.length);
+        const indices = [];
+        const currentPos = p.time;
+        const eachHeadOffset = p.length / c.count;
+        for (let i = 0; i < c.count; i++)
+            indices.push(quantize(currentPos, c.period, p.length, eachHeadOffset * i));
         return {
-            index, 
+            indices,
         };
     },
     pixel: (p, c, g) => {
-        let dist = g.index - p.index;
-        if (dist < 0) {
-            const distFromEnd = p.length - p.index - 1;
-            dist = g.index + distFromEnd;
-        }
-        let pos = 1;
-        if (dist > 0 && dist <= c.trail)
-            pos = dist / c.trail;
+        const potentialPositions = g.indices.map(i => {
+            let dist = i - p.index;
+            if (dist < 0) {
+                const distFromEnd = p.length - p.index - 1;
+                dist = i + distFromEnd;
+            }
+            let pos = 1;
+            if (dist > 0 && dist <= c.trail)
+                pos = dist / c.trail;
+            return pos;
+        });
+        const pos = Math.min(...potentialPositions);
 
         // return lerpRGBasYUV(c.primaryColor, c.secondaryColor, pos);
         return lerpRGB(c.primaryColor, c.secondaryColor, pos);
