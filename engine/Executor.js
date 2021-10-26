2b1a379b43cc35f824432ce37a9742817bac1ea6
Will Stott <willstott101@gmail.com>
2021-10-26 22:46:09 +0100

    On/Off button works - uses brightness.

--- engine/Executor.js
+++ engine/Executor.js
@@ -11,6 +11,7 @@ export class Executor {
         this._interval_ms = (1 / targetFps) * 1000;
         this._startTime = this._now();
         this._stopTime = undefined;
+        this._maxBrightness = 1;
     }
 
     addView(name, view) {
@@ -30,28 +31,64 @@ export class Executor {
     }
 
     execute(time) {
-        if (time === undefined) {
-            time = (new Date()).getTime() / 1000;
+        if (this._maxBrightness > 0) {
+            if (time === undefined) {
+                time = (new Date()).getTime() / 1000;
+            }
+            const p = {
+                time: time,
+                random: Math.random(),
+                length: this.pixelMap.length,
+            };
+            let g;
+            if (this._patch.global)
+                g = this._patch.global(p, this._config);
+            for (let i = 0; i < p.length; i++) {
+                const pixelData = this.pixelMap[i];
+                const color = this._patch.pixel(Object.assign(p, {
+                    pos: pixelData,
+                    index: i,
+                }), this._config, g);
+                const j = i * 3;
+                this.data[j + 0] = color.r;
+                this.data[j + 1] = color.g;
+                this.data[j + 2] = color.b;
+            }
+
+            if (this._maxBrightness < 1) {
+                this._reduceBrightness();
+            }
+        } else {
+            this._clear();
         }
-        const p = {
-            time: time,
-            random: Math.random(),
-            length: this.pixelMap.length,
-        };
-        let g;
-        if (this._patch.global)
-            g = this._patch.global(p, this._config);
-        for (let i = 0; i < p.length; i++) {
-            const pixelData = this.pixelMap[i];
-            const color = this._patch.pixel(Object.assign(p, {
-                pos: pixelData,
-                index: i,
-            }), this._config, g);
-            const j = i * 3;
-            this.data[j + 0] = color.r;
-            this.data[j + 1] = color.g;
-            this.data[j + 2] = color.b;
+
+    }
+
+    _clear() {
+        for (let i = 0; i <= this.data.length; i++)
+            this.data[i] = 0;
+    }
+
+    _reduceBrightness() {
+        const max = this._maxBrightness * 255 * 3;
+        for (let p = 0; p <= this.pixelMap.length; p++) {
+            const i = p * 3;
+            const total = this.data[i] + this.data[i + 1] + this.data[i + 2];
+            if (total > max) {
+                const scale = max / total;
+                this.data[i] *= scale;
+                this.data[i + 1] *= scale;
+                this.data[i + 2] *= scale;
+            }
         }
+    }
+
+    get maxBrightness() {
+      return this._maxBrightness;
+    }
+
+    set maxBrightness(val) {
+      return this._maxBrightness = val;
     }
 
     _now() {
--- frontend/App.svelte
+++ frontend/App.svelte
@@ -15,6 +15,30 @@
         else
             executor.stop();
     }
+
+    let off = false;
+    let brightness = 1;
+
+    function updateBrightness () {
+        if (off)
+            executor.maxBrightness = 0;
+        else
+            executor.maxBrightness = brightness;
+    }
+
+    function toggleOff () {
+        off = !off;
+        updateBrightness();
+    }
+
+    function brightnessChanged(newBrightness) {
+        brightness = newBrightness;
+        if (brightness <= 0)
+            off = true;
+        else
+            off = false;
+        updateBrightness();
+    }
 </script>
 
 <style>
@@ -49,12 +73,8 @@
             /*grid-row-start: 1;*/
             /*grid-row-end: 1;*/
         ">
-            <Button on:mousedown={togglePause} enabled={running} iconSize="small">
-                {#if running}
-                    <FaPowerOff/>
-                {:else}
-                    <FaPowerOff/>
-                {/if}
+            <Button on:mousedown={toggleOff} enabled={!off} iconSize="small">
+                <FaPowerOff/>
             </Button>
         </div>
         <div style="
@@ -65,9 +85,9 @@
         ">
             <Button on:mousedown={togglePause} enabled={running}>
                 {#if running}
-                    <MdPlay/>
-                {:else}
                     <MdStop/>
+                {:else}
+                    <MdPlay/>
                 {/if}
             </Button>
         </div>
