1670a5fe0d8e8c8a71aaadca420e89ca5046368b
Jacob212 <jacobhale.hale@gmail.com>
2021-10-28 21:53:28 +0100

    Removed move option

--- patterns/sineWave.js
+++ patterns/sineWave.js
@@ -5,7 +5,6 @@ export default {
     config: {
         period: 10,
         scale: 10,
-        move: true,
         thickness: 2,
         primaryColor: {
             r: 255,
@@ -26,9 +25,6 @@ export default {
     },
     pixel: (p, c, g) => {
         let pos = 1
-        if (!c.move) {
-            g.index = 0
-        }
         const maths = c.scale*Math.sin((p.pos.x+g.index)/c.scale)+12
         if (maths > p.pos.y - c.thickness && maths < p.pos.y + c.thickness) {
             pos = 0.5
