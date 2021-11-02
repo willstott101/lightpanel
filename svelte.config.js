c35d9828e2f2f1a54e9fe804e2510fa16008165d
Will Stott <willstott101@gmail.com>
2021-11-02 19:04:06 +0000

    grr

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,18 @@
 var sass = require('sass');
 
+console.log("BEFORE SASS EXPORTED");
+
 module.exports = {
   preprocess: {
     style: ({content, filename}) => {
+      console.log("BEFORE SASS", arguments);
       return new Promise((success, failure) => {
+        console.log("STARTING SASS", err, result);
         sass.render({
           data: content,
           file: filename,
         }, (err, result) => {
+          console.log("AFTER SASS", err, result);
           if (err)
             failure(err);
           else
