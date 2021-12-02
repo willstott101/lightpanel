a374de973b7502f72c018ed49416f56213c348dd
Will Stott <willstott101@gmail.com>
2021-12-02 21:24:58 +0000

    Fix sineWave's timing jank from quantize

--- patterns/sineWave.js
+++ patterns/sineWave.js
@@ -3,7 +3,7 @@ import { quantize } from "../engine/math.js";
 
 export default {
     config: {
-        period: 10,
+        speed: 20,
         scale: 10,
         thickness: 2,
         primaryColor: {
@@ -17,15 +17,9 @@ export default {
             b: 70,
         },
     },
-    global: (p, c) => {
-        let index = quantize(p.time, c.period, p.length);
-        return {
-            index,
-        };
-    },
     pixel: (p, c, g) => {
         let pos = 1
-        const maths = c.scale*Math.sin((p.pos.x+g.index)/c.scale)+12
+        const maths = c.scale*Math.sin((p.pos.x+p.time * c.speed)/c.scale)+12
         if (maths > p.pos.y - c.thickness && maths < p.pos.y + c.thickness) {
             pos = 0.5
         }
