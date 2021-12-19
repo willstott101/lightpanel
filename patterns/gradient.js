92e682c7cb28df42bbaf4a88443c577c43becefd
Will Stott <willstott101@gmail.com>
2021-12-19 22:08:30 +0000

    Rotate gradient - and add a commented animated gradient wipe

--- patterns/gradient.js
+++ patterns/gradient.js
@@ -7,7 +7,11 @@ export default {
         // angle: 0,
     },
     pixel: (p, c, g) => {
-        const pos = remap(p.pos.x, 0, p.width, 0, 1);
+        // const y = Math.round(p.pos.y / 2 + p.time * 2) % p.height;
+        // let pos = remap(y, 0, p.height, 0, 2);
+        // if (pos > 1) pos = 2 - pos;
+        // return evenGrad(p.palette, pos);
+        const pos = remap(p.pos.y, 0, p.height, 0, 1);
         return evenGrad(p.palette, pos);
     }
 };
