23ba54a43faed14e9821b6d08813a56c7972c319
Will Stott <willstott101@gmail.com>
2024-11-24 00:16:53 +0000

    brighter clouds

--- patterns/clouds.js
+++ patterns/clouds.js
@@ -14,7 +14,7 @@ export default {
         };
     },
     pixel: (p, c, g) => {
-        const pos = g.s(p.time / 20 + p.pos.y / 5, p.time / 20 + p.pos.x / 50, p.time / 10) + 0.2;
+        const pos = g.s(p.time / 20 + p.pos.y / 5, p.time / 20 + p.pos.x / 50, p.time / 10) + 0.3;
         return lerpRGB(p.palette.off, p.palette.on, Math.sqrt(pos));
     }
 };
